b5b201caa6054621c5b8260e115aee7981d85b44
garritfra <garritfranke@gmail.com>
2020-04-06 21:27:57 +0200

    Add default background

--- src/views/TodaysChallengeView.js
+++ src/views/TodaysChallengeView.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, ImageBackground} from 'react-native';
 
 export default function() {
   return (
-    <View style={styles.container}>
-      <View style={styles.todaysChallengeContainer}>
-        <Text style={styles.todaysChallengeText}>Deine heutiges Abenteuer</Text>
+    <ImageBackground
+      source={require('../assets/tagebuch.png')}
+      style={styles.background}>
+      <View style={styles.container}>
+        <View style={styles.todaysChallengeContainer}>
+          <Text style={styles.todaysChallengeText}>
+            Deine heutiges Abenteuer
+          </Text>
+        </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+  },
   container: {
     flex: 1,
     flexDirection: 'column',
@@ -23,12 +34,13 @@ const styles = StyleSheet.create({
     padding: 15,
     borderRadius: 30,
     color: '#666',
-    backgroundColor: '#eaeaea',
+    backgroundColor: '#EAEAEA9A',
     alignItems: 'center',
   },
   todaysChallengeText: {
     textAlign: 'center',
     fontSize: 34,
+    opacity: 1,
     fontWeight: 'bold',
     lineHeight: 36,
   },
